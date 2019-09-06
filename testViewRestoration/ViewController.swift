10f2dfa74716f068e13dd72ef7768b1222e94268
James Tang <james@goodnotesapp.com>
2019-09-06 12:56:47 +0800

    Initial Commit

---
+++ testViewRestoration/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  testViewRestoration
+//
+//  Created by James Tang on 6/9/2019.
+//  Copyright © 2019 James Tang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
